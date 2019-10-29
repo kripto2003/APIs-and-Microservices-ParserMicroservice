e2f127dbeb5a3ec38a9c7b845d36f9b0978e535e
kripto2003 <34577057+kripto2003@users.noreply.github.com>
2019-10-29 16:57:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,13 +18,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.ip, language: req.acceptsLanguages(), software: req.header('User-Agent')});
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
